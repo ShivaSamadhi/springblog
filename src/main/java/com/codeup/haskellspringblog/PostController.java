129e5dcacb0ad9141aaa86decf7e49ac57952464
Ramaj Johnson <rjohnson3795@gmail.com>
2022-11-17 16:02:23 -0600

    controllers spring

---
+++ src/main/java/com/codeup/haskellspringblog/PostController.java
@@ -0,0 +1,33 @@
+package com.codeup.haskellspringblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PostController {
+    @GetMapping("/posts")
+    @ResponseBody
+    public String posts(){
+        return "posts index page";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String singlePost(){
+        return "view an individual post";
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String viewCreate(){
+        return "view the form for creating a post";
+    }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String createPosts(){
+        return "create a new post";
+    }
+}
