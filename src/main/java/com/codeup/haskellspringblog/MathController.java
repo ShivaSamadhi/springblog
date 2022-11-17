b58452db5b1ff75546e8e43c4b482a44f918a42b
Ramaj Johnson <rjohnson3795@gmail.com>
2022-11-17 15:43:47 -0600

    controllers spring

--- src/main/java/com/codeup/haskellspringblog/MathController.java
+++ src/main/java/com/codeup/haskellspringblog/MathController.java
@@ -9,17 +9,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class MathController {
     @GetMapping("/add/{num1}/and/{num2}")
     @ResponseBody
-    public double add(@PathVariable int num1, @PathVariable int num2){}
+    public double add(@PathVariable int num1, @PathVariable int num2){
+        return (double) num1 + num2;
+    }
 
     @GetMapping("/subtract/{num1}/from/{num2}")
     @ResponseBody
-    public double subtract(@PathVariable int num1, @PathVariable int num2){}
+    public double subtract(@PathVariable int num1, @PathVariable int num2){
+        return (double) num1 - num2;
+    }
 
     @GetMapping("/multiply/{num1}/and/{num2}")
     @ResponseBody
-    public double multiply(){}
+    public double multiply(@PathVariable int num1, @PathVariable int num2){
+        return (double) num1 * num2;
+    }
 
-    @GetMapping
+    @GetMapping("/divide/{num1}/by/{num2}")
     @ResponseBody
-    public double divide(){}
+    public double divide(@PathVariable int num1, @PathVariable int num2){
+        return (double) num1 / num2;
+    }
 }
