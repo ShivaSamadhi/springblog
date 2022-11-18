a4d9e5c6eeb89d743eeaa6af870c2957c0a6f18b
Ramaj Johnson <rjohnson3795@gmail.com>
2022-11-18 15:53:14 -0600

    views w/thymeleaf

--- src/main/java/com/codeup/haskellspringblog/Post.java
+++ src/main/java/com/codeup/haskellspringblog/Post.java
@@ -1,12 +1,20 @@
 package com.codeup.haskellspringblog;
 
 public class Post {
+    private long id;
     private String title;
     private String body;
 
     public Post(){}
 
     public Post(String title, String body) {
+        this.title = title;
+        this.body = body;
+
+    }
+
+    public Post(long id, String title, String body) {
+        this.id = id;
         this.title = title;
         this.body = body;
     }
@@ -25,5 +33,13 @@ public class Post {
 
     public void setBody(String body) {
         this.body = body;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 }
--- src/main/java/com/codeup/haskellspringblog/PostController.java
+++ src/main/java/com/codeup/haskellspringblog/PostController.java
@@ -1,22 +1,31 @@
 package com.codeup.haskellspringblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
     @GetMapping("/posts")
-    @ResponseBody
-    public String posts(){
-        return "posts index page";
+    public String posts(Model model){
+        List<Post> allPosts = new ArrayList<>();
+        Post newPost = new Post(1, "Swag", "Hopped up out the bed. Turned my swag on. Took a look in the mirror, said wassup.");
+        allPosts.add(newPost);
+        model.addAttribute("allPosts", allPosts);
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String singlePost(){
-        return "view an individual post";
+    public String singlePost(@PathVariable long id, Model model){
+        Post singlePost = new Post(id, "Another One", "It goes on and on. Cant understand how I code so long.");
+        model.addAttribute("post", singlePost);
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
