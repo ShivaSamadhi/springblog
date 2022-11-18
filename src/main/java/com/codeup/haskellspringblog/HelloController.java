5bd0f4e742c50f3fccc7a90c97bfaf62cb36f6d0
Ramaj Johnson <rjohnson3795@gmail.com>
2022-11-18 10:24:40 -0600

    views w/thymeleaf

--- src/main/java/com/codeup/haskellspringblog/HelloController.java
+++ src/main/java/com/codeup/haskellspringblog/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -14,4 +16,14 @@ public class HelloController {
         return "hello";
     }
 
+    @GetMapping("/join")
+    public String showJoinForm(){
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model){
+        model.addAttribute("cohort", "Welcome to "+ cohort+ "!");
+        return "join";
+    }
 }
