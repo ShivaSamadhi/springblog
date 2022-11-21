0228a699f162ae5eb9ef83e25625be3c23c28c4e
Ramaj Johnson <rjohnson3795@gmail.com>
2022-11-21 14:01:21 -0600

    relationships

--- src/main/java/com/codeup/haskellspringblog/models/AdImage.java
+++ src/main/java/com/codeup/haskellspringblog/models/AdImage.java
@@ -1,4 +1,17 @@
 package com.codeup.haskellspringblog.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name ="ad_images")
 public class AdImage {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String path;
+
+    @ManyToOne
+    @JoinColumn(name = "ad_id")
+    private Ad ad;
 }
