1a0c40a04f01de94eaa830cd0605982c9e87389e
Ramaj Johnson <rjohnson3795@gmail.com>
2022-11-21 10:59:25 -0600

    repos & jpa

---
+++ src/main/java/com/codeup/haskellspringblog/HaskellSpringblogApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.haskellspringblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaskellSpringblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaskellSpringblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeup/haskellspringblog/HaskellSpringblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.haskellspringblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaskellSpringblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
