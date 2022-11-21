8822f2f112d1f2f0b0eac0cfcaca2870a5b87099
Ramaj Johnson <rjohnson3795@gmail.com>
2022-11-21 11:17:40 -0600

    repos & jpa

--- src/main/java/com/codeup/haskellspringblog/AdController.java
+++ src/main/java/com/codeup/haskellspringblog/AdController.java
@@ -1,4 +1,11 @@
 package com.codeup.haskellspringblog;
 
 public class AdController {
+    private final AdRepository adDao;
+
+    public AdController(AdRepository adDao) {
+        this.adDao = adDao;
+    }
+
+    //In Java, before we can use methods of other classes, we first need to create the object of that class (i.e. class A needs to create an instance of class B). One of the ways we can do this, is in the constructor. Spring provides us with some facilities to use automatic dependency injection in our classes. The following example shows how we can inject AdRepository into our AdController.
 }
