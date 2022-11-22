59bbe3af14540410e0501b816b65315e6d7426fc
Ramaj Johnson <rjohnson3795@gmail.com>
2022-11-22 09:40:54 -0600

    spring blog

--- src/main/java/com/codeup/haskellspringblog/repositories/UserRepository.java
+++ src/main/java/com/codeup/haskellspringblog/repositories/UserRepository.java
@@ -4,4 +4,5 @@ import com.codeup.haskellspringblog.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
