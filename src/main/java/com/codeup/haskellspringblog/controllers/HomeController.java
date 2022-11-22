daa82190dbc86d41cb656f73049d01b3a91224f1
Ramaj Johnson <rjohnson3795@gmail.com>
2022-11-22 08:52:17 -0600

    relationships

--- src/main/java/com/codeup/haskellspringblog/controllers/HomeController.java
+++ src/main/java/com/codeup/haskellspringblog/controllers/HomeController.java
@@ -1,15 +1,38 @@
 package com.codeup.haskellspringblog.controllers;
 
+import com.codeup.haskellspringblog.models.Ad;
+import com.codeup.haskellspringblog.repositories.AdRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
+
+    private final AdRepository adsDao;
+    // Anytime this controller is used, inject the adsDao so we can use it!
+    public HomeController(AdRepository adsDao) {
+        this.adsDao = adsDao;
+    }
     @GetMapping("/home")
-    public String welcome(){
+    public String welcome() {
         return "home";
     }
 
 
+    @GetMapping("/ads")
+    public String allAds(Model model) {
+        List<Ad> ads = adsDao.findAll();
+        for (Ad ad : ads) {
+            System.out.println("This ad title: " + ad.getTitle());
+            System.out.println("This ad description: " + ad.getDescription());
+            System.out.println("Owner: " + ad.getOwner().getFirstName() + " " + ad.getOwner().getLastName());
+        }
+        model.addAttribute("ads", ads);
+
+        return "ads";
+    }
 }
--- src/main/java/com/codeup/haskellspringblog/models/Ad.java
+++ src/main/java/com/codeup/haskellspringblog/models/Ad.java
@@ -35,4 +35,61 @@ public class Ad {
     )
     private List<AdCategory> categories;
 
+    public Ad(long id, String title, String description, User owner, List<AdImage> images, List<AdCategory> categories) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.owner = owner;
+        this.images = images;
+        this.categories = categories;
+    }
+    public Ad(){}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public List<AdImage> getImages() {
+        return images;
+    }
+
+    public void setImages(List<AdImage> images) {
+        this.images = images;
+    }
+
+    public List<AdCategory> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<AdCategory> categories) {
+        this.categories = categories;
+    }
 }
